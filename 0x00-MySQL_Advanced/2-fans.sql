471756d39ede23bb651a54194eedca6292515f74
lagatpetr <lagatpetr@gmail.com>
2024-06-13 14:04:46 +0000

    updated

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,6 @@
--- creates a table users
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT, 
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL,
-    PRIMARY KEY (id)
-);
+--  ranks country origins of bands, ordered by the number of (non-unique) fans
+SELECT origin AS origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
+
